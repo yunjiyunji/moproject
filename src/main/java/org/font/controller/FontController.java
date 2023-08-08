4b3ae8fa90221c76dd1b0ad1b2209ea092e200db
yunji <ywwi720@gmail.com>
2023-08-08 19:59:21 +0900

    메인화면 접속 controller

--- src/main/java/org/font/controller/FontController.java
+++ src/main/java/org/font/controller/FontController.java
@@ -1,13 +1,25 @@
 package org.font.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
+@Controller
 @Log4j
+@RequestMapping("/font/*")
+@AllArgsConstructor
 public class FontController {
 
-	@RequestMapping("")
-	public void gitTest() {
-		log.info("Git Test");
+	// 일반메인
+	@GetMapping("/main")
+	public String getMain() {
+
+		log.info("main 이동");
+
+		return "main";
 	}
 }
