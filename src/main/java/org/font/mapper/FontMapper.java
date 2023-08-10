0f3cd9fba2b05c040335a70163b36692c5353a93
yunji <ywwi720@gmail.com>
2023-08-11 01:14:43 +0900

    mapper select 구현

---
+++ src/main/java/org/font/mapper/FontMapper.java
@@ -0,0 +1,11 @@
+package org.font.mapper;
+
+import java.util.List;
+
+import org.font.domain.FontVO;
+
+public interface FontMapper {
+	
+	public List<FontVO> getList();
+	
+}
---
+++ src/test/java/org/font/mapper/FontMapperTests.java
@@ -0,0 +1,29 @@
+package org.font.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
+@Log4j
+public class FontMapperTests {
+
+	
+	@Setter(onMethod_ = { @Autowired })
+	private FontMapper fontmapper;
+	
+	@Test
+	public void testGetList() {
+		fontmapper.getList().forEach(font -> log.info(font));
+	}
+}
